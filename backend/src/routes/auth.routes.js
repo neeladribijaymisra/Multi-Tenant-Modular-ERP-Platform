8e451a6783da77b497ab00ac8d107cf7c2bcf2d9
Ashutosh Pradhan <ashupradhanctc@gmail.com>
2026-04-01 00:50:23 +0530

    Removed frontend/backend and kept only user modules

--- backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { login } from "../controllers/auth.controller.js";
-
-const router = Router({ mergeParams: true });
-
-router.post("/login", login);
-
-export default router;
